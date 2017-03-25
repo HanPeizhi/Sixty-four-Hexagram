9bb0d341d7676333bd252a88c249385290cb08e4
no reason <urqiang@gmail.com>
2017-03-25 09:37:30 +0800

    Add files via upload

---
+++ chk_in_64gua.py
@@ -0,0 +1,163 @@
+# 正常输入：
+#
+# 0. 乾之不动                            v
+# 1. 乾 & 乾不动                         v
+#
+# 2. 乾 坤 & 乾为天 坤为地                v
+#
+# 3. 乾之坤 & 天之地                     v
+# 4. 乾为天之坤为地 & 乾为天之坤          v
+# 5. 乾为天 之 坤 & 乾为天 之 地为坤      v
+#
+# (用strip：删除两端的空格)
+#
+# 乾
+# 天
+# 乾为天
+#
+# python 不能表达式赋值，就是不能在if()里赋值，这点感觉不错
+#
+# python 里没有switch，要用if-else和字典，还有其他方法实现
+# 这点我觉得不太好
+#
+#
+# 乾为1 坤为2， 输入 1之2 感觉不方便
+# 考虑用个符号代替之字， . 或者 -， 英文是点 中文是句号，不过减号中英相同
+#
+# 从软件界面输入得到的是数字，还得转换成字符
+# 这里还有个问题，如果是数字和中文混合输入，到时做出界面了在调试
+#
+# 'QIAN' 'Qian' 'qIan' 'qiAN' ben bian gua检查是不是字母，是则全转成小写
+# 之也用减号代替-，不然很麻烦
+
+__author__ = 'hanpeizhi'
+import re
+import lookup_dic
+
+# from lookup_dic import to_bi
+
+instr_gua = '1 之 2 '
+
+(ben_gua, bian_gua) = ('', '')  # 可不申明
+(bin_ben_gua, bin_bian_gua) = ('', '')
+
+instr_gua = instr_gua.strip()  # 检测前删除两端的空格
+
+
+def get_benbian_gua():
+    global ben_gua, bian_gua
+    ben_gua = match_str.group(1).rstrip()
+    bian_gua = match_str.group(2).lstrip()
+
+
+def get_bin_benbian():
+    global bin_ben_gua, bin_bian_gua
+    bin_ben_gua = lookup_dic.to_bin(ben_gua)
+    bin_bian_gua = lookup_dic.to_bin(bian_gua)
+
+
+match_str = re.match('(.*)之(.*)', instr_gua)  # '乾之坤'
+
+if match_str:
+    get_benbian_gua()
+    get_bin_benbian()
+    if ben_gua == bian_gua or bian_gua == '不动':  # '乾之不动'
+        bian_gua = ''
+        get_bin_benbian()
+else:  # 1 - 2 or 1 - 
+    match_str = re.match('\s*(\d+)\s*-*\s*(\d*)\s*', instr_gua)
+    if match_str:
+        get_benbian_gua()
+        get_bin_benbian()
+    else:
+        
+
+
+    '''
+    match_str = re.match('(.*)\s+(.*)', instr_gua)  # '乾 坤'
+    if match_str:
+        get_benbian_gua()
+        get_bi_benbian()
+    else:
+        if re.search('不动', instr_gua):  # '乾不动'
+            match_str = re.match('(.*)不动(.*)', instr_gua)  # if match_str:
+            get_benbian_gua()
+            get_bi_benbian()
+        else:  # '乾'
+            ben_gua = instr_gua
+            get_bi_benbian()
+            print('to check the dic')  # 如果字典里查不到就是输入错误
+        '''
+print('本卦:', ben_gua)
+print('变卦:', bian_gua)
+print(bin_ben_gua, bin_bian_gua)
+
+
+
+
+'''
+语法
+################################################
+#0. 乾之不动
+#3. 乾之坤 & 天之地
+#4. 乾为天之地为坤  
+
+>>> import re
+>>> match = re.match('(.*)之(.*)', '乾之不动')
+>>> match.group(1)
+'乾'
+>>> match.group(2)
+'不动'
+>>> match.groups()
+('乾', '不动')
+>>> match.group(0)
+'乾之不动'
+
+>>> match = re.match('(.*)之(.*)', '乾之')
+>>> match
+<_sre.SRE_Match object; span=(0, 2), match='乾之'>
+>>> match.groups()
+('乾', '')
+
+
+################################################
+#2. 乾 坤
+
+>>> match = re.match('(.*)\s+(.*)', '乾 坤')
+>>> match.groups()
+('乾', '坤')
+
+>>> re.split(r'\s+', '乾 坤')
+['乾', '坤']
+
+################################################
+#1. 乾 & 乾不动 
+
+>>> if re.search('不动', '乾不动'):
+	print('1')
+
+	
+1
+
+>>> match = re.match('(.*)不动(.*)', '乾不动')
+>>> match
+<_sre.SRE_Match object; span=(0, 3), match='乾不动'>
+>>> match.groups()
+('乾', '')
+
+#最后才是只有一个乾字的时候再直接查字典，如果查不到就报错
+
+################################################
+
+#5. 用strip：删除两端的空格
+
+>>> s = '   乾   '
+>>> s.strip()
+'乾'
+
+################################################
+
+#...
+#to be continued...
+looks like done...
+'''
---
+++ lookup_dic.py
@@ -0,0 +1,207 @@
+# coding:utf-8
+# qian -> 111 111
+#
+# 履 -> lv0 因为履在上经，所以排第一
+# 旅 -> lv1 因为旅在下经，所以排第二
+#
+# 因为懒不想动脑子折腾，就直接用if-elif-else来实现下面的功能
+
+__author__ = 'hanpeizhi'
+
+#in＿name = ''
+#out_binary = ''
+#global out_binary
+
+
+def to_bin(in＿name):  # in_name 不能同时是全局变量和函数元素
+    global out_binary
+    if in＿name == '乾' or in_name == '乾为天' \
+            or in＿name == 'qian' \
+            or in_name == '天' or in＿name == 'tian' \
+            or in＿name == '乾乾' or in＿name == 'qianqian'\
+            or in_name == '111111' or in＿name == '1':  # 1
+        out_binary = '111111'
+        return out_binary
+
+    elif in＿name == '坤' or in＿name == '坤为地' \
+            or in＿name == 'kun' \
+            or in＿name == '地' or in＿name == 'di' \
+            or in＿name == '坤坤' or in＿name == 'kunkun' \
+            or in＿name == '000000' or in＿name == '2':  # 2
+        out_binary = '000000'
+        return out_binary
+
+    elif in＿name == '屯' or in＿name == '水雷屯' \
+            or in＿name == 'tun' or in＿name == 'zun' or in＿name == 'zhun' \
+            or in＿name == '水雷' or in＿name == 'shuilei' or in＿name == 'suilei' \
+            or in＿name == '坎震' or in＿name == 'kanzhen' or in＿name == 'kanzen' \
+            or in＿name == 'kanzheng' or in＿name == 'kanzeng' \
+            or in＿name == '010001':  # 3
+        out_binary = '010001'
+        return out_binary
+
+    elif in＿name == '蒙' or in＿name == '山水' or in＿name == '山水蒙' \
+            or in＿name == '100010' or in＿name == '艮坎' \
+            or in＿name == 'meng' or in＿name == 'men' \
+            or in＿name == 'genkan' or in＿name == 'gengkan' \
+            or in＿name == 'shanshui' or in＿name == 'sansui' \
+            or in＿name == 'shansui' or in＿name == 'sanshui':  # 4
+        out_binary = '100010'
+        return out_binary
+
+    elif in＿name == '需' or in＿name == '水天需' or in＿name == '水天' \
+            or in＿name == '010111'or in＿name == '坎乾'\
+            or in＿name == 'xu'\
+            or in＿name == 'kanqian'\
+            or in＿name == 'shuitian' or in＿name == 'suitian':  # 5
+        out_binary = '010111'
+        return out_binary
+
+    elif in＿name == '讼' or in＿name == '天水讼' or in＿name == '天水'\
+            or in＿name == '111010' or in＿name == '乾坎'\
+            or in＿name == 'song' or in＿name == 'son' or in＿name == 'shon'or in＿name == 'shong' \
+            or in＿name == 'qiankan' \
+            or in＿name == 'tianshui' or in＿name == 'tiansui':  # 6
+        out_binary = '111010'
+        return out_binary
+
+    elif in＿name == '师' or in＿name == '地水师' \
+            or in＿name == 'shi' or in＿name == 'si' \
+            or in＿name == '地水' or in＿name == 'dishui' or in＿name == 'disui'\
+            or in＿name == '坤坎' or in＿name == 'kunkan' \
+            or in＿name == '000010':  # 7
+        out_binary = '000010'
+        return out_binary
+
+    elif in＿name == '比' or in＿name == '水地比' \
+            or in＿name == 'bi' \
+            or in＿name == '水地' or in＿name == 'shuidi' or in＿name == 'suidi' \
+            or in＿name == '坎坤' or in＿name == 'kankun' \
+            or in＿name == '010000':  # 8
+        out_binary = '010000'
+        return out_binary
+
+    elif in＿name == '小畜' or in＿name == '风天小畜' \
+            or in＿name == 'xiaochu' or in＿name == 'xiaocu'\
+            or in＿name == '风天' or in＿name == 'fengtian' or in＿name == 'fentian' \
+            or in＿name == '巽乾' or in＿name == 'xunqian' \
+            or in＿name == '110111':  # 9
+        out_binary = '110111'
+        return out_binary
+
+    elif in＿name == '履' or in＿name == '天泽履' \
+            or in＿name == 'lv0' \
+            or in＿name == '天泽' or in＿name == 'tianze' or in＿name == 'tianzhe' \
+            or in＿name == '乾兑' or in＿name == 'qiandui' \
+            or in＿name == '111011':  # 10
+        out_binary = '111011'
+        return out_binary
+
+    elif in＿name == '泰' or in＿name == '地天泰' \
+            or in＿name == 'tai' \
+            or in＿name == '地天' or in＿name == 'ditian' \
+            or in＿name == '坤乾' or in＿name == 'kunqian' \
+            or in＿name == '000111':  # 11
+        out_binary = '000111'
+        return out_binary
+
+    elif in＿name == '否' or in＿name == '天地否' \
+            or in＿name == 'pi' or in＿name == 'fou' \
+            or in＿name == '天地' or in＿name == 'tiandi' \
+            or in＿name == '乾坤' or in＿name == 'qiankun' \
+            or in＿name == '111000':  # 12
+        out_binary = '111000'
+        return out_binary
+
+    elif in＿name == '同人' or in＿name == '天火同人' \
+            or in＿name == 'tongren' or in＿name == 'tonren' \
+            or in＿name == '天火' or in＿name == 'tianhuo' \
+            or in＿name == '乾离' or in＿name == 'qianli' \
+            or in＿name == '111101':  # 13
+        out_binary = '111101'
+        return out_binary
+
+    elif in＿name == '大有' or in＿name == '火天大有' \
+            or in＿name == 'dayou' \
+            or in＿name == '火天' or in＿name == 'huotian' \
+            or in＿name == '离乾' or in＿name == 'liqian' \
+            or in＿name == '101111':  # 14
+        out_binary = '101111'
+        return out_binary
+
+    elif in＿name == '谦' or in＿name == '地山谦' \
+            or in＿name == 'qian' \
+            or in＿name == '地山' or in＿name == 'dishan' or in＿name == 'disan' \
+            or in＿name == '坤艮' or in＿name == 'kungen' or in＿name == 'kungeng' \
+            or in＿name == '000100':  # 15
+        out_binary = '000100'
+        return out_binary
+
+    elif in＿name == '豫' or in＿name == '雷地豫' or in＿name == '0010000':  # 16
+        out_binary = '001000'
+        return out_binary
+
+    elif in＿name == '随' or in＿name == '泽雷随' or in＿name == '011001':  # 17
+        out_binary = '011001'
+        return out_binary
+
+    elif in＿name == '蛊' or in＿name == '山风蛊' or in＿name == '100110':  # 18
+        out_binary = '100110'
+        return out_binary
+
+    elif in＿name == '临' or in＿name == '地泽临' or in＿name == '000011':  # 19
+        out_binary = '000011'
+        return out_binary
+
+    elif in＿name == '观' or in＿name == '风地观' or in＿name == '110000':  # 20
+        out_binary = '110000'
+        return out_binary
+
+    elif in＿name == '噬嗑' or in＿name == '火雷噬嗑' or in＿name == '101001':  # 21
+        out_binary = '101001'
+        return out_binary
+
+    elif in＿name == '贲' or in＿name == '山火贲' or in＿name == '100101':  # 22
+        out_binary = '100101'
+        return out_binary
+
+    elif in＿name == '剥' or in＿name == '山地剥' or in＿name == '100000':  # 23
+        out_binary = '100000'
+        return out_binary
+
+    elif in＿name == '复' or in＿name == '地雷复' or in＿name == '000001':  # 24
+        out_binary = '000001'
+        return out_binary
+
+    elif in＿name == '无妄' or in＿name == '天雷无妄' or in＿name == '111001':  # 25
+        out_binary = '111001'
+        return out_binary
+
+    elif in＿name == '大畜' or in＿name == '山天大畜' or in＿name == '100111':  # 26
+        out_binary = '100111'
+        return out_binary
+
+    elif in＿name == '颐' or in＿name == '山雷颐' or in＿name == '100001':  # 27
+        out_binary = '100001'
+        return out_binary
+
+    elif in＿name == '大过' or in＿name == '泽风大过' or in＿name == '011110':  # 28
+        out_binary = '011110'
+        return out_binary
+
+    elif in＿name == '水' or in＿name == '坎为水' or in＿name == '坎' or in＿name == '010010':  # 29
+        out_binary = '010010'
+        return out_binary
+
+    elif in＿name == '火' or in＿name == '离为火' or in＿name == '离' or in＿name == '1011011':  # 30  上经
+        out_binary = '101101'
+        return out_binary
+
+    elif in＿name == '':
+        out_binary = ''
+        return out_binary
+    else:
+        return "不要调皮！"
+
+# lookup_dic(in＿name)
+#print(out_binary, in＿name)
