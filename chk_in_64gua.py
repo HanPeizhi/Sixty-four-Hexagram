e58b765707c21edc34f846d35e8be704c32084ca
HanPeizhi <urqiang@gmail.com>
2017-03-25 20:24:15 +0800

    better

--- chk_in_64gua.py
+++ chk_in_64gua.py
@@ -11,7 +11,7 @@
 #
 # python 不能表达式赋值，就是不能在if()里赋值，这点感觉不错
 # python 里没有switch，要用if-else和字典，还有其他方法实现
-# 
+#
 #
 # 乾为1 坤为2， 输入 1之2 感觉不方便
 # 考虑用个符号代替之字， . 或者 -， 英文是点 中文是句号，不过减号中英相同
@@ -19,7 +19,7 @@
 # 从软件界面输入得到的是数字，还得转换成字符
 # 这里还有个问题，如果是数字和中文混合输入，到时做出界面了在调试
 #
-# 
+#
 # 还有有一种算法，就是search函数，search 之，zhi，- 这三个关键字符
 # 关键字符之前是本卦，关键字符之后为变卦
 #
@@ -29,17 +29,17 @@ import re
 import lookup_dic
 # from lookup_dic import to_bi
 
-instr_gua =  '乾之'
+instr_gua = '乾之'
 (ben_gua, bian_gua) = ('', '')  # 可不申明
 (bin_ben_gua, bin_bian_gua) = ('', '')
 
-#instr_gua = instr_gua.strip()  # 检测前删除两端的空格
+# instr_gua = instr_gua.strip()  # 检测前删除两端的空格
 
 
 def get_benbian_gua():
     global ben_gua, bian_gua
-    ben_gua = match_str.group(1).lower()  #.rstrip()
-    bian_gua = match_str.group(2).lower()  #.lstrip()
+    ben_gua = match_str.group(1).lower()  # .rstrip()
+    bian_gua = match_str.group(2).lower()  # .lstrip()
 
 
 def get_bin_benbian():
@@ -51,12 +51,14 @@ def get_bin_benbian():
 # 两个卦名必须全中文 或者 拼音和数字
 # 目前只匹配阿拉伯数字和小写拼音 #'\s*([a-zA-Z0-9]+)\s*-*[Zz][h]*i*之*\s*([a-zA-Z0-9]*)\s*'
 # '1 - 2' or '1 - ' or '1' or '11 之 1'  # zhi 不行
-match_str = re.match(r'\s*([a-zA-Z0-9]+)\s*[-之\s*]*\s*([a-zA-Z0-9]*)\s*', instr_gua)
+match_str = re.match(
+    r'\s*([a-zA-Z0-9]+)\s*[-之\s*]*\s*([a-zA-Z0-9]*)\s*', instr_gua)
 if match_str:
     get_benbian_gua()
     get_bin_benbian()
-else:   
-    match_str = re.match(r'\s*([\u4e00-\u9fa5]+)\s*[-之\s*]\s*([\u4e00-\u9fa5]*)\s*', instr_gua)
+else:
+    match_str = re.match(
+        r'\s*([\u4e00-\u9fa5]+)\s*[-之\s*]\s*([\u4e00-\u9fa5]*)\s*', instr_gua)
     if match_str:
         get_benbian_gua()
         get_bin_benbian()
@@ -71,16 +73,16 @@ print('变卦:', bian_gua)
 print(bin_ben_gua, bin_bian_gua)
 
 
-
 '''
-if re.search('不动', instr_gua) and not re.search('之', instr_gua) and not re.search('-', instr_gua):  # '乾不动'
-        match_str = re.match('\s*(.+)\s*不动\s*(.*)\s*', instr_gua)  # if match_str:
+if re.search('不动', instr_gua) and not re.search('之', instr_gua)\
+    and not re.search('-', instr_gua):  # '乾不动'
+        match_str = re.match('\s*(.+)\s*不动\s*(.*)\s*',
+        instr_gua)  # if match_str:
         get_benbian_gua()
         get_bin_benbian()
-    else:   #' 乾 - 不动 ' or '乾之不动' or ' 乾 之 不动 ' or '乾 坤'  or '乾为天' or '乾为天之不动'  or '乾为天 之 不动' 
-'''
+    else:   #' 乾 - 不动 ' or '乾之不动' or ' 乾 之 不动 ' or '乾 坤'
+            #  or '乾为天' or '乾为天之不动'  or '乾为天 之 不动'
 
-'''
 match_str = re.match('\s*(.+)\s*之*\s*(.*)\s*', instr_gua)       # '乾之坤'
 
 if match_str:
@@ -89,7 +91,7 @@ if match_str:
     if ben_gua == bian_gua or bian_gua == '不动':   # '乾之不动'
         bian_gua = ''
         get_bin_benbian()
-else:  
+else:
     match_str = re.match('(.*)\s+(.*)', instr_gua)  # '乾 坤'
     if match_str:
         get_benbian_gua()
@@ -117,7 +119,7 @@ else:
 ################################################
 #0. 乾之不动
 #3. 乾之坤 & 天之地
-#4. 乾为天之地为坤  
+#4. 乾为天之地为坤
 
 >>> import re
 >>> match = re.match('(.*)之(.*)', '乾之不动')
@@ -148,12 +150,11 @@ else:
 ['乾', '坤']
 
 ################################################
-#1. 乾 & 乾不动 
+#1. 乾 & 乾不动
 
 >>> if re.search('不动', '乾不动'):
-	print('1')
+    print('1')
 
-	
 1
 
 >>> match = re.match('(.*)不动(.*)', '乾不动')
